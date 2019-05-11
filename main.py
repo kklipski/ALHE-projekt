fe9b825441d271342e447280044d19febae1da8c
krzepka <rzarol.kepka@gmail.com>
2019-05-11 22:53:24 +0200

    Skrypt do uczenia sieci ddpg

---
+++ main.py
@@ -0,0 +1,49 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+# noinspection PyUnresolvedReferences
+from ddpg_agent import Agent
+
+
+
+def ddpg(n_episodes=2000, max_t=700):
+    scores_deque = deque(maxlen=100)
+    scores = []
+    max_score = -np.Inf
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        agent.reset()
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_deque.append(score)
+        scores.append(score)
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
+        if i_episode % 100 == 0:
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+    return scores
+
+if __name__=="__main__":
+    env = gym.make('BipedalWalker-v2')
+    env.seed(10)
+    agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.shape[0], random_seed=10)
+
+    scores = ddpg()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(1, len(scores)+1), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
