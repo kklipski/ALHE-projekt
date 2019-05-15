142a0b4ff7670f04fcbd02b58b0f3cb85a6bdaef
krzepka <rzarol.kepka@gmail.com>
2019-05-15 17:11:43 +0200

    Reformat kodu, dodanie klasy sieci ewolucyjnej

--- src/ddpg/main.py
+++ src/ddpg/main.py
@@ -1,75 +1,63 @@
 from __future__ import division
+
+import gc
 import gym
 import numpy as np
-import torch
-from torch.autograd import Variable
-import os
-import psutil
-import gc
 
-import train
-import buffer
+from src.ddpg.buffer import MemoryBuffer
+from src.ddpg.train import Trainer
 
-env = gym.make('BipedalWalker-v2')
-# env = gym.make('Pendulum-v0')
+def main():
+    env = gym.make('BipedalWalker-v2')
 
-MAX_EPISODES = 5000
-MAX_STEPS = 1000
-MAX_BUFFER = 1000000
-MAX_TOTAL_REWARD = 300
-S_DIM = env.observation_space.shape[0]
-A_DIM = env.action_space.shape[0]
-A_MAX = env.action_space.high[0]
+    MAX_EPISODES = 5000
+    MAX_STEPS = 1000
+    MAX_BUFFER = 1000000
+    MAX_TOTAL_REWARD = 300
+    S_DIM = env.observation_space.shape[0]
+    A_DIM = env.action_space.shape[0]
+    A_MAX = env.action_space.high[0]
 
-print(' State Dimensions :- ', S_DIM)
-print(' Action Dimensions :- ', A_DIM)
-print(' Action Max :- ', A_MAX)
+    print(' State Dimensions :- ', S_DIM)
+    print(' Action Dimensions :- ', A_DIM)
+    print(' Action Max :- ', A_MAX)
 
-ram = buffer.MemoryBuffer(MAX_BUFFER)
-trainer = train.Trainer(S_DIM, A_DIM, A_MAX, ram)
+    ram = MemoryBuffer(MAX_BUFFER)
+    trainer = Trainer(S_DIM, A_DIM, A_MAX, ram)
 
-for _ep in range(MAX_EPISODES):
-	observation = env.reset()
-	print('EPISODE :- ', _ep)
-	for r in range(MAX_STEPS):
-		env.render()
-		state = np.float32(observation)
+    for _ep in range(MAX_EPISODES):
+        observation = env.reset()
+        print('EPISODE :- ', _ep)
+        for r in range(MAX_STEPS):
+            env.render()
+            state = np.float32(observation)
 
-		action = trainer.get_exploration_action(state)
-		# if _ep%5 == 0:
-		# 	# validate every 5th episode
-		# 	action = trainer.get_exploitation_action(state)
-		# else:
-		# 	# get action based on observation, use exploration policy here
-		# 	action = trainer.get_exploration_action(state)
+            action = trainer.get_exploration_action(state)
+            new_observation, reward, done, info = env.step(action)
 
-		new_observation, reward, done, info = env.step(action)
+            if done:
+                new_state = None
+            else:
+                new_state = np.float32(new_observation)
+                # push this exp in ram
+                ram.add(state, action, reward, new_state)
 
-		# # dont update if this is validation
-		# if _ep%50 == 0 or _ep>450:
-		# 	continue
+            observation = new_observation
 
-		if done:
-			new_state = None
-		else:
-			new_state = np.float32(new_observation)
-			# push this exp in ram
-			ram.add(state, action, reward, new_state)
+            # perform optimization
+            trainer.optimize()
+            if done:
+                break
 
-		observation = new_observation
+        # check memory consumption and clear memory
+        gc.collect()
+        # process = psutil.Process(os.getpid())
+        # print(process.memory_info().rss)
 
-		# perform optimization
-		trainer.optimize()
-		if done:
-			break
+        if _ep % 100 == 0:
+            trainer.save_models(_ep)
 
-	# check memory consumption and clear memory
-	gc.collect()
-	# process = psutil.Process(os.getpid())
-	# print(process.memory_info().rss)
+    print('Completed episodes')
 
-	if _ep%100 == 0:
-		trainer.save_models(_ep)
-
-
-print('Completed episodes')
+if __name__ == "__main__":
+    main()
---
+++ src/ddpg_evo/evo.py
@@ -0,0 +1,49 @@
+from src.ddpg.train import Trainer
+from src.ddpg.buffer import MemoryBuffer
+import gym
+
+class EvolutionaryDDPG:
+    def __init__(self, n_networks, max_buffer):
+        self.n = n_networks
+        self.ddpgs = self.create_ddpg()
+        self.envs = self.create_envs()
+        self.max_buffer = max_buffer
+
+    def exploit(self):
+        pass
+
+    def explore(self):
+        pass
+
+    def create_envs(self):
+        envs = []
+        for i in range(self.n):
+            env = gym.make('BipedalWalker-v2')
+            envs.append(env)
+
+        return envs
+    def create_ddpg(self):
+        ddpgs = []
+        for i in range(self.n):
+            env = self.envs[i]
+            S_DIM = env.observation_space.shape[0]
+            A_DIM = env.action_space.shape[0]
+            A_MAX = env.action_space.high[0]
+
+            print(' State Dimensions :- ', S_DIM)
+            print(' Action Dimensions :- ', A_DIM)
+            print(' Action Max :- ', A_MAX)
+
+            ram = MemoryBuffer(self.max_buffer)
+            trainer = Trainer(S_DIM, A_DIM, A_MAX, ram)
+
+            ddpgs.append(trainer)
+        return ddpgs
+
+    def train(self):
+        pass
+    def load_ckpt(self):
+        pass
+    def save_ckpt(self):
+        pass
+
