63c20f000f79d540d64c68b125714e5f75b6994c
krzepka <rzarol.kepka@gmail.com>
2019-05-15 23:58:18 +0200

    Added test_evo

---
+++ src/ddpg_evo/test_evo.py
@@ -0,0 +1,10 @@
+from src.ddpg_evo.evo import EvolutionaryDDPG
+
+def main():
+    evo_network = EvolutionaryDDPG(n_networks = 3, max_buffer = 100000,
+                                   max_episodes = 2, max_steps = 200, episodes_ready = 4)
+
+    evo_network.train()
+
+if __name__ == "__main__":
+    main()
