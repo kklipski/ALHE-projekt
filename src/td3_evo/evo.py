6ceae11c919b09f6044d50758d2607b892207efb
krzepka <rzarol.kepka@gmail.com>
2019-06-01 15:07:55 +0200

    New exploit, some fixes

--- src/td3_evo/evo.py
+++ src/td3_evo/evo.py
@@ -7,10 +7,12 @@ import gym
 import numpy as np
 import random
 import scipy.stats
+import itertools
 
 
 class EvolutionaryTD3:
-    def __init__(self, n_networks, max_buffer, max_episodes, max_steps, episodes_ready, explore_prob, explore_factors):
+    def __init__(self, n_networks, max_buffer, max_episodes, max_steps, episodes_ready, explore_prob, explore_factors,
+                 saved_scores=10, stop_condition=100):
         self.n = n_networks  # liczba sieci
         self.max_buffer = max_buffer
         self.max_episodes = max_episodes
@@ -27,12 +29,12 @@ class EvolutionaryTD3:
         self.lr = 0.001
 
         # początkowe ostatnie 10 cząstkowych wyników dla wszystkich sieci ustawiamy na -100
-        self.last_ten_scores = [[-100 for _ in range(10)] for _ in range(self.n)]
+        self.last_ten_scores = [[-100 for _ in range(saved_scores)] for _ in range(self.n)]
 
         self.envs = self.create_envs()
         self.policies = self.create_policies()
         self.replay_buffers = self.create_replay_buffers()
-        self.stop_condition = 150
+        self.stop_condition = stop_condition
 
 
 
@@ -70,6 +72,20 @@ class EvolutionaryTD3:
         for param in self.policies[idx].critic_2_target.parameters():
             param.data.mul_(factor)
 
+    def copy_weights(self, model_from, model_to, picking):
+        indexes = []
+        for i in range(picking):
+            r = random.randint(0, 2)
+
+            while r in indexes:  # żeby się indeksy nie powtarzały
+                r = random.randint(0, 2)
+
+            indexes.append(2 * r)  # 0,2,4
+
+        for idx, (param_to, param_from) in enumerate(zip(model_to.parameters(), model_from.parameters())):
+            if idx in indexes or idx - 1 in indexes:  # 0 w indexes dla 0,1 tutaj, 2 w indexes dla 2,3 tutaj, 4 w indexes dla 4,5 tutaj
+                param_to.data.copy_(param_from)
+
     def exploit(self, idx):
         """
         Eksploatacja polega na jednolitym próbkowaniu innego (losowo wybranego) agenta w populacji,
@@ -92,107 +108,27 @@ class EvolutionaryTD3:
         if idx != best_net_idx:
             # kopiujemy tylko część wag z nowej sieci - im większa jest różnica między średnimi cząstkowymi nagrodami
             # dla rozpatrywanych sieci, tym więcej wag zostanie podmienionych (ich ilość zmienia się progowo)
+
+            # kopiujemy 1 albo 2 wylosowane tensory
             if 0 <= mean_diff < 30:
-                threshold = 0.33
+                to_pick = 1
             else:
-                threshold = 0.66
+                to_pick = 2
 
-            # podmieniamy wagi
-            condition = 0  # -1 - do not copy bias, 0 - weight, 1 - copy bias
+            self.copy_weights(self.policies[best_net_idx].actor, self.policies[idx].actor, to_pick)
+            self.copy_weights(self.policies[best_net_idx].actor_target, self.policies[idx].actor_target, to_pick)
+            self.copy_weights(self.policies[best_net_idx].critic_1, self.policies[idx].critic_1, to_pick)
+            self.copy_weights(self.policies[best_net_idx].critic_1_target, self.policies[idx].critic_1_target, to_pick)
+            self.copy_weights(self.policies[best_net_idx].critic_2, self.policies[idx].critic_2, to_pick)
+            self.copy_weights(self.policies[best_net_idx].critic_2_target, self.policies[idx].critic_2_target, to_pick)
 
-            new_param = self.policies[best_net_idx].actor.parameters()
-            for param in self.policies[idx].actor.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            new_param = self.policies[best_net_idx].actor_target.parameters()
-            for param in self.policies[idx].actor.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            new_param = self.policies[best_net_idx].critic_1.parameters()
-            for param in self.policies[idx].critic_1.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            new_param = self.policies[best_net_idx].critic_1_target.parameters()
-            for param in self.policies[idx].critic_1.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            new_param = self.policies[best_net_idx].critic_2.parameters()
-            for param in self.policies[idx].actor.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            new_param = self.policies[best_net_idx].critic_2_target.parameters()
-            for param in self.policies[idx].actor.parameters():
-                if condition == -1:
-                    next(new_param)
-                    condition = 0
-                elif condition == 0:
-                    if random.random() < threshold:
-                        param.data.copy_(next(new_param))
-                        condition = 1
-                    else:
-                        condition = -1
-                else:
-                    param.data.copy_(next(new_param))
-                    condition = 0
-
-            print("<exploit", idx, "> Wczytano nowe wagi z sieci nr ", best_net_idx)
+            print("<exploit", idx, "> Wczytano nowe wagi z sieci nr ", best_net_idx, random_idx, "\t",
+                  mean(self.last_ten_scores[idx]), " vs. ",
+                  mean(self.last_ten_scores[best_net_idx]))
         else:
-            print("<exploit", idx, "> Wagi zostają, obecne są lepsze od sieci nr ", random_idx)
+            print("<exploit", idx, "> Wagi zostają, obecne są lepsze od sieci nr ", random_idx, "\t",
+                  mean(self.last_ten_scores[idx]), " vs. ",
+                  mean(self.last_ten_scores[best_net_idx]))
 
     def explore(self, idx):
 
@@ -238,10 +174,11 @@ class EvolutionaryTD3:
         noise_clip = 0.5
         policy_delay = 2            # delayed policy updates parameter
         max_timesteps = 2000        # max timesteps in one episode
-        directory = "./preTrained/{}".format(env_name) # save trained models
+        directory = "./preTrained/"  # save trained models
         filename = "TD3_{}_{}".format(env_name, random_seed)
         ###################################
 
+        log_f = open("log.txt", "w+")
         # Liczba iteracji algorytmu
         for episode in range(self.max_episodes):
 
@@ -250,13 +187,12 @@ class EvolutionaryTD3:
                 # logging variables:
                 avg_reward = 0
                 ep_reward = 0
-                log_f = open("log.txt","w+")
 
                 state = self.envs[idx].reset()
                 no_reward_counter = -100  # jeśli dojdzie do 100 uznajemy, że robot się nie porusza
                 start_no_reward_counter = 0
                 for t in range(max_timesteps):
-                    self.envs[idx].render()
+                    # self.envs[idx].render()
                     # select action and add exploration noise:
                     action = self.policies[idx].select_action(state)
                     action = action + np.random.normal(0, exploration_noise, size=self.envs[idx].action_space.shape[0])
@@ -268,12 +204,12 @@ class EvolutionaryTD3:
                     # jeśli robot stoi w miejscu przez stop_condition kroków, zakończ pętlę
                     if no_reward_counter == 0:
                         start_no_reward_counter = reward
+                        # print("START=",start_no_reward_counter)
                     if start_no_reward_counter-0.05 < reward < start_no_reward_counter+0.05:
+                        # print("COUNTER=",no_reward_counter)
                         no_reward_counter += 1
                     else:
                         no_reward_counter = 0
-                    if no_reward_counter > self.stop_condition:
-                        break
 
                     self.replay_buffers[idx].add((state, action, reward, next_state, float(done)))
                     state = next_state
@@ -282,9 +218,12 @@ class EvolutionaryTD3:
                     ep_reward += reward
 
                     # if episode is done then update policy:
-                    if done or t==(max_timesteps-1):
+                    if done or t == (max_timesteps - 1) or no_reward_counter > self.stop_condition:
+                        # print("UPDATING POLICES...")
                         self.policies[idx].update(self.replay_buffers[idx], t, batch_size, gamma, polyak, policy_noise, noise_clip, policy_delay)
                         break
+
+                self.append_score(idx, ep_reward)
 
                 # każda sieć ma swój max epizodów, po których zostaną wywołane metody exploit i explore
                 if episode % self.episodes_ready[idx] == 0 and episode != 0:
@@ -293,7 +232,7 @@ class EvolutionaryTD3:
                         self.explore(idx)
 
                 # logging updates:
-                log_f.write('{},{}\n'.format(episode, ep_reward))
+                log_f.write('{},{},{}\n'.format(idx, episode, ep_reward))
                 log_f.flush()
                 ep_reward = 0
 
