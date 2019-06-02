e65c183636859270146760ef2a95f11017976ae0
krzepka <K.Rzepka@stud.elka.pw.edu.pl>
2019-06-02 15:08:58 +0200

    Solo td3 correction

--- src/td3/train.py
+++ src/td3/train.py
@@ -7,7 +7,7 @@ from src.td3.utils import ReplayBuffer
 def train():
     ######### Hyperparameters #########
     env_name = "BipedalWalker-v2"
-    log_interval = 10           # print avg reward after interval
+    log_interval = 1           # print avg reward after interval
     random_seed = 0
     gamma = 0.99                # discount for future rewards
     batch_size = 100            # num of transitions sampled from replay buffer
@@ -19,8 +19,9 @@ def train():
     policy_delay = 2            # delayed policy updates parameter
     max_episodes = 1000         # max num of episodes
     max_timesteps = 2000        # max timesteps in one episode
-    directory = "./preTrained/{}".format(env_name) # save trained models
+    directory = "./preTrained/" # save trained models
     filename = "TD3_{}_{}".format(env_name, random_seed)
+    stop_condition = 100
     ###################################
     
     env = gym.make(env_name)
@@ -45,7 +46,10 @@ def train():
     # training procedure:
     for episode in range(1, max_episodes+1):
         state = env.reset()
+        no_reward_counter = -100  # jeśli dojdzie do 100 uznajemy, że robot się nie porusza
+        start_no_reward_counter = 0
         for t in range(max_timesteps):
+            env.render()
             # select action and add exploration noise:
             action = policy.select_action(state)
             action = action + np.random.normal(0, exploration_noise, size=env.action_space.shape[0])
@@ -53,6 +57,44 @@ def train():
             
             # take action in env:
             next_state, reward, done, _ = env.step(action)
+
+            # jeśli robot stoi w miejscu przez stop_condition kroków, zakończ pętlę
+            if no_reward_counter == 0:
+                start_no_reward_counter = reward
+                # print("START=",start_no_reward_counter)
+            if start_no_reward_counter - 0.05 < reward < start_no_reward_counter + 0.05:
+                # print("COUNTER=",no_reward_counter)
+                no_reward_counter += 1
+            else:
+                no_reward_counter = 0
+
+            # jeśli robot stoi w miejscu dłuższy czas, ustaw flagę
+            stand_flag = no_reward_counter > stop_condition
+
+            if stand_flag:
+                stuck_pen_flag = True
+                if ep_reward <= 0:
+                    reward = reward + -100
+                    stuck_pen = -100
+                elif 0 < ep_reward <= 50:
+                    reward = reward + -49
+                    stuck_pen = -49
+                elif 50 < ep_reward <= 100:
+                    reward = reward + -36
+                    stuck_pen = -36
+                elif 100 < ep_reward <= 150:
+                    reward = reward + -25
+                    stuck_pen = -25
+                elif 150 < ep_reward <= 200:
+                    reward = reward + -16
+                    stuck_pen = -16
+                elif 200 < ep_reward <= 250:
+                    reward = reward + -9
+                    stuck_pen = -9
+                else:
+                    reward = reward + -4
+                    stuck_pen = -4
+
             replay_buffer.add((state, action, reward, next_state, float(done)))
             state = next_state
             
@@ -60,7 +102,7 @@ def train():
             ep_reward += reward
             
             # if episode is done then update policy:
-            if done or t==(max_timesteps-1):
+            if done or t == (max_timesteps-1):
                 policy.update(replay_buffer, t, batch_size, gamma, polyak, policy_noise, noise_clip, policy_delay)
                 break
         
@@ -76,16 +118,16 @@ def train():
             policy.save(directory, name)
             log_f.close()
             break
-        
-        if episode > 500:
+
+        if episode % 100 == 0 and episode != 0:
             policy.save(directory, filename)
-        
+
         # print avg reward every log interval:
         if episode % log_interval == 0:
             avg_reward = int(avg_reward / log_interval)
             print("Episode: {}\tAverage Reward: {}".format(episode, avg_reward))
             avg_reward = 0
 
+
 if __name__ == '__main__':
     train()
-    
